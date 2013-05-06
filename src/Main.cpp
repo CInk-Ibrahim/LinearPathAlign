5bb0d9f86833bb2e7cb57c37c775ce6d0eec4b03
Tinithraviel <tinithraviel@gmail.com>
2013-05-07 00:02:57 +0300

    dsf

--- src/Main.cpp
+++ src/Main.cpp
@@ -26,7 +26,7 @@ int main() {
 	node v8 = G.new_node();
 	node v9 = G.new_node();
 
-	//G.new_edge(v2, v1);
+	G.new_edge(v2, v1);
 	G.new_edge(v3, v1);
 	G.new_edge(v3, v4);
 	G.new_edge(v2, v7);
@@ -52,10 +52,11 @@ int main() {
 	cout << A[v6] << " - " << A[v7] << " - " << A[v8] << " - " << A[v9] << endl;
 	cout << "LA Score : " << la_score(G, A) << endl;
 
-	//simulated_annealing(G,A);
+	simulated_annealing(G, A);
 
-	//cout << A[v1] <<" - " << A[v2] <<" - " << A[v3] <<" - " << A[v4] <<" - " << A[v5] <<" - " <<endl;
-	//	cout << A[v6] <<" - " << A[v7] <<" - " << A[v8] <<" - " << A[v9] <<endl;
-	//	cout << "LA Score : " << la_score(G, A) << endl;
+	cout << A[v1] << " - " << A[v2] << " - " << A[v3] << " - " << A[v4] << " - "
+			<< A[v5] << " - " << endl;
+	cout << A[v6] << " - " << A[v7] << " - " << A[v8] << " - " << A[v9] << endl;
+	cout << "LA Score : " << la_score(G, A) << endl;
 
 }
