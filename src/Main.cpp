c4677ccd5dae62320e9aed440585a71aad81774d
Tinithraviel <tinithraviel@gmail.com>
2013-05-07 01:27:18 +0300

    License changes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,7 @@
 /*-------------------------------------------------------------------------------
  * Copyright (c) 2013 Ibrahim Suzer.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- *
- * Contributors:
- *     Ibrahim Suzer - initial API and implementation
+ * All rights reserved.  Distributed under the terms of the GNU Public License v3.0
+ * which can be found at "license.txt" or at: http://www.gnu.org/licenses/gpl.html
  * -------------------------------------------------------------------------------*/
 
 #include "Include.h"
@@ -48,7 +43,7 @@ int main() {
 	simulated_annealing(G, A);
 
 	cout << A[v1] << " - " << A[v2] << " - " << A[v3] << " - " << A[v4] << " - "
-			<< A[v5] << " - " << endl;
+	<< A[v5] << " - " << endl;
 	cout << A[v6] << " - " << A[v7] << " - " << A[v8] << " - " << A[v9] << endl;
 	cout << "LA Score : " << la_score(G, A) << endl;
 
